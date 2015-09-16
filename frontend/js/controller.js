ad6c73e1ca78a5c6432a9e99fb4843c9fcf771b9
MichelleKay5 <mkherschell@satx.rr.com>
2015-09-16 14:45:04 -0500

    Update controller.js

--- frontend/js/controller.js
+++ frontend/js/controller.js
@@ -1,14 +1,26 @@
   angular.module('airwaysApp', [])
     .controller('SearchFormController', ['$scope', function($scope, API) {
-      $scope.master = {};
-
-      $scope.update = function(user) {
-        $scope.master = angular.copy(user);
-      };
-
-      $scope.reset = function() {
-        $scope.user = angular.copy($scope.master);
-      };
-
-      $scope.reset();
+      $scope.flightList = [
+    {
+      Flight: {
+        price: '$300.00',
+        departure: '6:00 am',
+        arrival: '3:00 pm',
+        origin: 'SAT',
+        destination: 'NYC',
+        duration: '8 hours',
+        number of seats available: '16',
+      }
     }]);
+
+
+
+  // $scope.update = function(user) {
+  //       $scope.master = angular.copy(user);
+  //     };
+
+  //     $scope.reset = function() {
+  //       $scope.user = angular.copy($scope.master);
+  //     };
+
+      // $scope.reset();
