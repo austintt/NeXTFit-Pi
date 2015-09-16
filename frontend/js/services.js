41514fb9bb7dc9a1bd01999d2793d1c74a1289ce
MichelleKay5 <mkherschell@satx.rr.com>
2015-09-16 16:46:42 -0500

    added the rest of the calls

--- frontend/js/services.js
+++ frontend/js/services.js
@@ -11,5 +11,47 @@ angular.module('airwaysApp.services', []).
       });
     }
     
+     raspberryAPI.getFlightDetails = function() {
+      return $http {{
+        method: 'JSONP',
+        url: 'localhost:3000/api'
+      });
+    }
+    
+     raspberryAPI.getOrderDetails = function() {
+      return $http {{
+        method: 'JSONP',
+        url: 'localhost:3000/api'
+      });
+    }
+    
+     raspberryAPI.setCheckIn = function() {
+      return $http {{
+        method: 'JSONP',
+        url: 'localhost:3000/api'
+      });
+    }
+    
+     raspberryAPI.setPaymentInfo = function() {
+      return $http {{
+        method: 'JSONP',
+        url: 'localhost:3000/api'
+      });
+    }
+    
+     raspberryAPI.setOrderDetails = function() {
+      return $http {{
+        method: 'JSONP',
+        url: 'localhost:3000/api'
+      });
+    }
+    
+     raspberryAPI.updateOrderDetails = function() {
+      return $http {{
+        method: 'JSONP',
+        url: 'localhost:3000/api'
+      });
+    }
+    
     return raspberryAPI;
   });
